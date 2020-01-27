409b11a4214a246985ecfc19ea1c9f844555936e
Sergitxu <sergiortiz@gmail.com>
2020-01-27 23:28:42 +0100

    Added services and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WeatherCardComponent } from './weather-card/weather-card.component';
+import { LoginComponent } from './pages/login/login.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AddComponent } from './pages/add/add.component';
+import { DetailsComponent } from './pages/details/details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    WeatherCardComponent,
+    AddComponent,
+    DetailsComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
